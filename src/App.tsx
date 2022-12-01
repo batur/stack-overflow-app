bfc3cbdf52fb42909f65c0b532e194bd78c5ee4c
sevvalsaygat <sevvallsstt@gmail.com>
2022-12-01 18:11:17 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<div>Home</div>} />
+      <Route path='/list' element={<div>List</div>} />
+    </Routes>
   );
 }
 
